950727137d76ed559d66a577d669e5af95c17c8f
marc <marc.glaser28@gmail.com>
2020-12-09 14:49:05 +0100

    added graph

--- TestApp/main.py
+++ TestApp/main.py
@@ -10,6 +10,7 @@ from linaccel import AndroidLinearAccelerometer
 from android.permissions import request_permissions, Permission
 from gyro import AndroidGyroscope
 from plyer import accelerometer
+from kivy.garden.graph import MeshLinePlot
 import datetime
 import os
 import threading
@@ -19,6 +20,16 @@ class AccelerometerTest(BoxLayout):
         super(AccelerometerTest, self).__init__(**kwargs)
         self.sensorEnabled = False
         self.linaccelEnabled = False
+        self.graph = self.ids.graph_plot
+
+        # For all X, Y and Z axes
+        self.plot = []
+        self.plot.append(MeshLinePlot(color=[1, 0, 0, 1]))  # X - Red
+        self.plot.append(MeshLinePlot(color=[0, 1, 0, 1]))  # Y - Green
+        self.plot.append(MeshLinePlot(color=[0, 0, 1, 1]))  # Z - Blue
+
+        self.reset_plots()
+
         request_permissions([Permission.WRITE_EXTERNAL_STORAGE, Permission.READ_EXTERNAL_STORAGE])
         try:
             Environment = autoclass("android.os.Environment")
@@ -37,6 +48,15 @@ class AccelerometerTest(BoxLayout):
         self.acc = AndroidLinearAccelerometer()
         self.gyro = AndroidGyroscope()
 
+        for plot in self.plot:
+            self.graph.add_plot(plot)
+            
+    def reset_plots(self):
+        for plot in self.plot:
+            plot.points = [(0, 0)]
+
+        self.counter = 1
+        
     def do_toggle(self):
         if not self.sensorEnabled:
             if self.linaccelEnabled:
@@ -124,34 +144,53 @@ class AccelerometerTest(BoxLayout):
             self.ids.accel_status.text = status
 
     def get_linearacceleration(self, dt):
+        if (self.counter == 100):
+            for plot in self.plot:
+                del(plot.points[0])
+                plot.points[:] = [(i[0] - 1, i[1]) for i in plot.points[:]]
+
+            self.counter = 99
+
         gyro = self.gyro.get_rotation()
         lin = self.acc.get_linearacceleration()
 
-        if not lin == (None, None, None):
-            self.ids.x_label.text = "X: " + str(lin[0])
+        if(not lin == (None, None, None)):
+            self.plot[0].points.append((self.counter, lin[0]))
             self.x_ro.append(gyro[0])
             self.x_accel.append(lin[0])
-            self.ids.y_label.text = "Y: " + str(lin[1])
+            self.plot[1].points.append((self.counter, lin[1]))
             self.y_ro.append(gyro[1])
             self.y_accel.append(lin[1])
-            self.ids.z_label.text = "Z: " + str(lin[2])
+            self.plot[2].points.append((self.counter, lin[2]))
             self.z_ro.append(gyro[2])
             self.z_accel.append(lin[2])
 
-    def get_acceleration(self, dt):
-        gyro = self.gyro.get_rotation()
-        accel = accelerometer.acceleration
+        self.counter += 1
 
-        if not accel == (None, None, None):
-            self.ids.x_label.text = "X: " + str(accel[0])
+
+    def get_acceleration(self, dt):
+        if (self.counter == 100):
+            for plot in self.plot:
+                del(plot.points[0])
+                plot.points[:] = [(i[0] - 1, i[1]) for i in plot.points[:]]
+
+            self.counter = 99
+
+        accel = accelerometer.acceleration
+        gyro = self.gyro.get_rotation()
+
+        if(not accel == (None, None, None)):
+            self.plot[0].points.append((self.counter, accel[0]))
             self.x_ro.append(gyro[0])
             self.x_accel.append(accel[0])
-            self.ids.y_label.text = "Y: " + str(accel[1])
+            self.plot[1].points.append((self.counter, accel[1]))
             self.y_ro.append(gyro[1])
             self.y_accel.append(accel[1])
-            self.ids.z_label.text = "Z: " + str(accel[2])
+            self.plot[2].points.append((self.counter, accel[2]))
             self.z_ro.append(gyro[2])
             self.z_accel.append(accel[2])
+
+        self.counter += 1
 
 
 class AccelerometerTestApp(App):
@@ -164,3 +203,4 @@ class AccelerometerTestApp(App):
 
 if __name__ == '__main__':
     AccelerometerTestApp().run()
+

4d4b10bdd0934ca4cff09806a326993e340f6ebc
Daniel Reith <dani.reith@web.de>
2021-01-14 20:00:01 +0100

    visualisation and setting implementation

--- NavigationDrawer/main.py
+++ NavigationDrawer/main.py
@@ -4,6 +4,8 @@ from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.uix.datatables import MDDataTable
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.textfield import MDTextFieldRect
+from kivymd.uix.selectioncontrol import MDSwitch
 from gyro import AndroidGyroscope
 from linaccel import AndroidLinearAccelerometer
 from kivy.clock import Clock
@@ -19,6 +21,7 @@ ScreenManager:
     MeasureScreen:
     DataScreen:
     OffsetScreen:
+        
 <MenuScreen>:
     name: 'menu'
     BoxLayout:
@@ -72,6 +75,7 @@ ScreenManager:
             ymin:-15
             ymax:20
         
+        
     MDRoundFlatIconButton:
         icon: "settings"
         text: "Offset & Gain"
@@ -81,7 +85,7 @@ ScreenManager:
     MDRectangleFlatButton:
         text: 'Start'
         pos_hint: {'center_x':0.2, 'center_y':0.13}
-        on_press: root.start_measurement()
+        on_press: root.start_button()
     MDRectangleFlatButton:
         text: 'Stop'
         pos_hint: {'center_x':0.5, 'center_y':0.13}
@@ -120,29 +124,31 @@ ScreenManager:
     name: 'offset'
 
     MDSwitch:
-        id: 'delay'
+        id: delay
         pos_hint: {'center_x': 0.1, 'center_y': 0.95}
     MDLabel:
         text: "Delay in s"
         pos_hint: {'center_x': 0.7, 'center_y': 0.95}
     MDTextFieldRect:
+        id: delay_value
         size_hint: 0.3, None
         height: "30dp"
         pos_hint: {'center_x': 0.7, 'center_y': 0.95}
 
     MDSwitch:
-        id: 'duration'
+        id: duration
         pos_hint: {'center_x': 0.1, 'center_y': 0.85}
     MDLabel:
         text: "Duration in s"
         pos_hint: {'center_x': 0.7, 'center_y': 0.85}
     MDTextFieldRect:
+        id: duration_value
         size_hint: 0.3, None
         height: "30dp"
         pos_hint: {'center_x': 0.7, 'center_y': 0.85}
 
     MDSwitch:
-        id: 'offset_lin'
+        id: offset_lin
         pos_hint: {'center_x': 0.1, 'center_y': 0.75}
     MDLabel:
         text: "Offset Linear"
@@ -151,6 +157,7 @@ ScreenManager:
         text: "X"
         pos_hint: {'center_x': 0.9, 'center_y': 0.7}
     MDTextFieldRect:
+        id: x_linoff
         size_hint: 0.3, None
         height: "30dp"
         pos_hint: {'center_x': 0.7, 'center_y': 0.7}
@@ -158,6 +165,7 @@ ScreenManager:
         text: "Y"
         pos_hint: {'center_x': 0.9, 'center_y': 0.6}
     MDTextFieldRect:
+        id: y_linoff
         size_hint: 0.3, None
         height: "30dp"
         pos_hint: {'center_x': 0.7, 'center_y': 0.6}
@@ -165,12 +173,13 @@ ScreenManager:
         text: "Z"
         pos_hint: {'center_x': 0.9, 'center_y': 0.5}
     MDTextFieldRect:
+        id: z_linoff
         size_hint: 0.3, None
         height: "30dp"
         pos_hint: {'center_x': 0.7, 'center_y': 0.5}
 
     MDSwitch:
-        id: 'offset_rot'
+        id: offset_rot
         pos_hint: {'center_x': 0.1, 'center_y': 0.4}
     MDLabel:
         text: "Offset Rotatorisch"
@@ -179,6 +188,7 @@ ScreenManager:
         text: "X"
         pos_hint: {'center_x': 0.9, 'center_y': 0.35}
     MDTextFieldRect:
+        id: x_rotoff
         size_hint: 0.3, None
         height: "30dp"
         pos_hint: {'center_x': 0.7, 'center_y': 0.35}
@@ -186,6 +196,7 @@ ScreenManager:
         text: "Y"
         pos_hint: {'center_x': 0.9, 'center_y': 0.25}
     MDTextFieldRect:
+        id: y_rotoff
         size_hint: 0.3, None
         height: "30dp"
         pos_hint: {'center_x': 0.7, 'center_y': 0.25}
@@ -193,6 +204,7 @@ ScreenManager:
         text: "Z"
         pos_hint: {'center_x': 0.9, 'center_y': 0.15}
     MDTextFieldRect:
+        id: z_rotoff
         size_hint: 0.3, None
         height: "30dp"
         pos_hint: {'center_x': 0.7, 'center_y': 0.15}
@@ -217,24 +229,24 @@ class MeasureScreen(Screen):
         Clock.schedule_once(self.init)
 
     def init(self, t):
-        self.graph = self.ids.graph_plot
-        #self.add_widget(self.graph)
+        self.acc_graph = self.ids.graph_plot
 
         # For all X, Y and Z axes
-        self.plot = []
-        self.plot.append(MeshLinePlot(color=[1, 0, 0, 1]))  # X - Red
-        self.plot.append(MeshLinePlot(color=[0, 1, 0, 1]))  # Y - Green
-        self.plot.append(MeshLinePlot(color=[0, 0, 1, 1]))  # Z - Blue
+        self.acc_plot = []
+        self.acc_plot.append(MeshLinePlot(color=[1, 0, 0, 1]))  # X - Red
+        self.acc_plot.append(MeshLinePlot(color=[0, 1, 0, 1]))  # Y - Green
+        self.acc_plot.append(MeshLinePlot(color=[0, 0, 1, 1]))  # Z - Blue
 
         self.reset_plots()
-        for plot in self.plot:
-            self.graph.add_plot(plot)
+        for plot in self.acc_plot:
+            self.acc_graph.add_plot(plot)
 
         self.counter = 1
 
     def reset_plots(self):
-        for plot in self.plot:
+        for plot in self.acc_plot:
             plot.points = [(0, 0)]
+
 
     def init_measurement(self):
         """"Request Permissions to write, set path to save files, instantiate sensors and
@@ -249,6 +261,19 @@ class MeasureScreen(Screen):
             os.mkdir(os.path.join(self.sdpath, "AccGyroData"))
         self.sdpath += "/AccGyroData"
 
+        self.linoffset = False
+        self.rotoffset = False
+        if self.manager.screens[3].ids["offset_lin"].active:
+            self.linoffset = True
+            self.x_offset_lin = float(self.manager.screens[3].ids["x_linoff"].text)
+            self.y_offset_lin = float(self.manager.screens[3].ids["y_linoff"].text)
+            self.z_offset_lin = float(self.manager.screens[3].ids["z_linoff"].text)
+        if self.manager.screens[3].ids["offset_rot"].active:
+            self.rotoffset = True
+            self.x_offset_rot = float(self.manager.screens[3].ids["x_rotoff"].text)
+            self.y_offset_rot = float(self.manager.screens[3].ids["y_rotoff"].text)
+            self.z_offset_rot = float(self.manager.screens[3].ids["z_rotoff"].text)
+
         self.x_rotation = []
         self.y_rotation = []
         self.z_rotation = []
@@ -258,10 +283,18 @@ class MeasureScreen(Screen):
         self.accelerometer = AndroidLinearAccelerometer()
         self.gyroscope = AndroidGyroscope()
 
-    def start_measurement(self):
+    def start_button(self):
         """"Start measurement. Calls init_measurement function and enables the sensors.
             Lists to save values are being cleared and a timer to read the sensor data gets started"""
         self.init_measurement()
+        if self.manager.screens[3].ids["delay"].active:
+            Clock.schedule_once(self.start_measurement, float(self.manager.screens[3].ids["delay_value"].text))
+        else:
+            self.start_measurement(t=0)
+
+    def start_measurement(self, t):
+        if self.manager.screens[3].ids["duration"].active:
+            Clock.schedule_once(self.stop_measurement_duration, float(self.manager.screens[3].ids["duration_value"].text))
         self.gyroscope.enable()
         self.accelerometer.enable()
         self.gyroscope.enable()
@@ -272,7 +305,14 @@ class MeasureScreen(Screen):
         self.x_rotation.clear()
         self.y_rotation.clear()
         self.z_rotation.clear()
+        self.counter = 1
+        self.reset_plots()
         Clock.schedule_interval(self.get_sensordata, 1 / 20)
+
+    def stop_measurement_duration(self, t):
+        self.gyroscope.disable()
+        self.accelerometer.disable()
+        Clock.unschedule(self.get_sensordata)
 
     def stop_measurement(self):
         """"Disable Sensors and unschedule get_sensordata function"""
@@ -301,7 +341,7 @@ class MeasureScreen(Screen):
     def get_sensordata(self, t):
         """Reads sensordata and appends them to the corresponding list every time the function gets called"""
         if (self.counter == 100):
-            for plot in self.plot:
+            for plot in self.acc_plot:
                 del (plot.points[0])
                 plot.points[:] = [(i[0] - 1, i[1]) for i in plot.points[:]]
 
@@ -310,16 +350,29 @@ class MeasureScreen(Screen):
         gyro = self.gyroscope.get_rotation()
         lin = self.accelerometer.get_linearacceleration()
 
-        if (not lin == (None, None, None)):
-            self.plot[0].points.append((self.counter, lin[0]))
-            self.x_rotation.append(gyro[0])
-            self.x_acceleration.append(lin[0])
-            self.plot[1].points.append((self.counter, lin[1]))
-            self.y_rotation.append(gyro[1])
-            self.y_acceleration.append(lin[1])
-            self.plot[2].points.append((self.counter, lin[2]))
-            self.z_rotation.append(gyro[2])
-            self.z_acceleration.append(lin[2])
+        if (not lin == (None, None, None) and (not gyro ==(None, None, None))):
+            if self.linoffset:
+                self.x_acceleration.append(lin[0] + self.x_offset_lin)
+                self.y_acceleration.append(lin[1] + self.y_offset_lin)
+                self.z_acceleration.append(lin[2] + self.z_offset_lin)
+                self.acc_plot[0].points.append((self.counter, lin[0] + self.x_offset_lin))
+                self.acc_plot[1].points.append((self.counter, lin[1] + self.y_offset_lin))
+                self.acc_plot[2].points.append((self.counter, lin[2] + self.z_offset_lin))
+            else:
+                self.x_acceleration.append(lin[0])
+                self.y_acceleration.append(lin[1])
+                self.z_acceleration.append(lin[2])
+                self.acc_plot[0].points.append((self.counter, lin[0]))
+                self.acc_plot[1].points.append((self.counter, lin[1]))
+                self.acc_plot[2].points.append((self.counter, lin[2]))
+            if self.rotoffset:
+                self.x_rotation.append(gyro[0] + self.x_offset_rot)
+                self.y_rotation.append(gyro[1] + self.y_offset_rot)
+                self.z_rotation.append(gyro[2] + self.z_offset_rot)
+            else:
+                self.x_rotation.append(gyro[0])
+                self.y_rotation.append(gyro[1])
+                self.z_rotation.append(gyro[2])
 
         self.counter += 1
 
